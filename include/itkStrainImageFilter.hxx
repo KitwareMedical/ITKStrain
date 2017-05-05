9e723e2a5735e1c4084e7be72b6977f3b02ae7fa
Matt McCormick <matt.mccormick@kitware.com>
2017-05-05 10:58:52 -0400

    BUG: Throw exception when transform not specified or invalid StrainForm

--- include/itkStrainImageFilter.hxx
+++ include/itkStrainImageFilter.hxx
@@ -87,6 +87,12 @@ StrainImageFilter< TInputImage, TOperatorValueType, TOutputValueType >
       }
     }
 
+  const StrainFormType strainForm = this->GetStrainForm();
+  if( strainForm != INFINITESIMAL && strainForm != GREENLAGRANGIAN && strainForm != EULERIANALMANSI )
+    {
+    itkExceptionMacro( "Invalid StrainForm!" );
+    }
+
   OutputImageType * output = this->GetOutput();
   output->FillBuffer( NumericTraits< OutputPixelType >::Zero );
 }
