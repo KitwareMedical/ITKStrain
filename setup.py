0c06666c028861aebb363157cb571a64f757ccbe
Matt McCormick <matt.mccormick@kitware.com>
2021-03-10 21:56:05 -0500

    ENH: Update CI for ITK 5.2 RC 3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-strain',
-    version='0.3.0',
+    version='0.3.1',
     author='Matthew M. McCormick',
     author_email='matt.mccormick@kitware.com',
     packages=['itk'],
@@ -53,6 +53,6 @@ setup(
     keywords='ITK Strain Tensor Displacement Field',
     url=r'https://github.com/InsightSoftwareConsortium/ITKStrain',
     install_requires=[
-        r'itk>=5.1.2'
+        r'itk>=5.2rc3'
     ]
     )
