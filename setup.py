5604083360e31980405694cc75a89db1eaa56f98
Tom Birdsong <tom.birdsong@kitware.com>
2021-08-26 11:39:48 -0400

    ENH: Update ITK CI version to 5.2.1

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     keywords='ITK Strain Tensor Displacement Field',
     url=r'https://github.com/InsightSoftwareConsortium/ITKStrain',
     install_requires=[
-        r'itk>=5.2.0.post2'
+        r'itk>=5.2.1.post1'
     ]
     )
