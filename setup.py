17a7a05f0e57a9a27419ad3530a9161c69e6b103
Tom Birdsong <tom.birdsong@kitware.com>
2021-09-02 11:24:49 -0400

    ENH: Update Python package version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-strain',
-    version='0.3.4',
+    version='0.3.5',
     author='Matthew M. McCormick',
     author_email='matt.mccormick@kitware.com',
     packages=['itk'],
